cdf4c504c1f271a82797e72177ecde97b4837499
Sn0tten <king.ferdows4@gmail.com>
2025-08-25 14:50:28 +0200

    Update oppgave1.php

--- tema01/oppgave1.php
+++ tema01/oppgave1.php
@@ -7,20 +7,10 @@
     <h3>oppgave1</h3>
 <form method="post" action="oppgave-1.php"id="oppgave1"name="oppgave1">
     fornavn<input type="text"id="fornavn"name="fornavn"required/><br/>
-    Etternavn<input type="text"id="etternavn"name="etternavn"required/><br/>
+    etternavn<input type="text"id="etternavn"name="etternavn"required/><br/>
     <input type="submit" value="Fortsett"id=fortsett name="fortsett"/>
     <input type="reset" value="Nullstill"id=nullstill name="nullstill"/><br/>
 </form>
 
 </body>
 </html>
-
-<?php
-
-$fornavn=$_POST["fornavn"];
-$etternavn=$_POST["etternavn"];
-
-
-print ("god dag$fornavn $etternavn<br/>");
-    
-?>
