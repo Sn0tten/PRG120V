d2358571b809593d08675cbc76156f4eba0f099c
Sn0tten <king.ferdows4@gmail.com>
2025-08-25 15:51:35 +0200

    ja

--- tema01/oppgave3.php
+++ tema01/oppgave3.php
@@ -3,12 +3,12 @@
 $number1=$_POST["number1"];
 $number2=$_POST["number2"];
 
-$sum=$number+$number2;
-$differanse=$number-$number2;
-$produkt=$number*$number2;
-$kvotient=$number/$number2;
-print ("$number + $number2 = $sum <br />");
-print ("$number - $number2 = $differanse <br />");
-print ("$number * $number2 = $produkt <br />");
-print ("$number / $number2 = $kvotient <br />");
+$sum=$number1+$number2;
+$differanse=$number1-$number2;
+$produkt=$number1*$number2;
+$kvotient=$number1/$number2;
+print ("$number1 + $number2 = $sum <br />");
+print ("$number1 - $number2 = $differanse <br />");
+print ("$number1 * $number2 = $produkt <br />");
+print ("$number1 / $number2 = $kvotient <br />");
 ?>
