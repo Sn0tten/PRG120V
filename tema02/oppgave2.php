f1c8cba47a25cb492e0f689b823ec7d7a51ba0b1
Sn0tten <king.ferdows4@gmail.com>
2025-08-25 15:29:05 +0200

    dd

--- tema02/oppgave2.php
+++ tema02/oppgave2.php
@@ -6,6 +6,6 @@ $alder=$_POST["alder"];
 /* hei  igjen*/
 /* hei 2 */
 
-print ("god dag $fornavn $etternavn<br />");
+print ("god dag $fornavn $etternavn du er like spreke på alderen $alder <br />");
     
 ?>
