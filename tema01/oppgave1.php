5e9b3202a5b8334eb9583c272a0b4f4da51e2824
Sn0tten <king.ferdows4@gmail.com>
2025-08-25 14:30:32 +0200

    opiawfojm

---
+++ tema01/oppgave1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>oppgave1.</title>
+</head>
+<body>
+    <h3>oppgave1</h3>
+<form method="post" action="oppgave-1.php"id="oppgave1"name="oppgave1">
+    fornavn<input type="text"id="fornavn"name="fornavn"required/><br/>
+    Etternavn<input type="text"id="etternavn"name="etternavn"required/><br/>
+    <input type="submit" value="Fortsett"id=fortsett name="fortsett"/>
+    <input type="reset" value="Nullstill"id=nullstill name="nullstill"/><br/>
+</form>
+
+</body>
+</html>
+
+<?php 
+$fornavn="";
+$etternavn="";
+
+print ("god dag$fornavn $etternavn<br/>");
+    
+?>
