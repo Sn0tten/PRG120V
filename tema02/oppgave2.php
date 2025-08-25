228027f847d5dbd6458cb625a6dad3813429a681
Sn0tten <king.ferdows4@gmail.com>
2025-08-25 15:28:10 +0200

    ww

---
+++ tema02/oppgave2.php
@@ -0,0 +1,11 @@
+<?php
+
+$fornavn=$_POST["fornavn"];
+$etternavn=$_POST["etternavn"];
+$alder=$_POST["alder"];
+/* hei  igjen*/
+/* hei 2 */
+
+print ("god dag $fornavn $etternavn<br />");
+    
+?>
