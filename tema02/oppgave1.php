c95fe2d78201eb790721b942282b4a55d4404f5d
Sn0tten <king.ferdows4@gmail.com>
2025-09-10 10:25:47 +0200

    ffsv

---
+++ tema02/oppgave1.php
@@ -0,0 +1,13 @@
+<?php
+$tall1=$_POST["number"];
+$tall2=$_POST["number2"];
+$svar=$_POST["svar"];
+
+if ($svar ==9)
+{
+    print("Riktig. 3 ganger 3 er 9");
+}
+else
+{
+    print("Feil. 3 ganger 3 er ikke $svar");
+}
