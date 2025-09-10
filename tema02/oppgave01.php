1f8d21881a5be33c1a03ebf9e109db12b809225b
Sn0tten <king.ferdows4@gmail.com>
2025-09-10 10:50:55 +0200

    dd

--- tema02/oppgave01.php
+++ tema02/oppgave01.php
@@ -1,15 +1,15 @@
-<?php
-$tall1=$_POST["tall1"];
-$tall2=$_POST["tall2"];
-if (isset($POST ["fortsett"]))
-
-    $svar =$_POST["svar"];
-if ($svar ==9)
+<?php /* Oppgave 1 */
+/*
+/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
+/* Programmet sjekker om svaret er riktig og skriver ut en melding avhengig av om svaret er riktig eller feil
+*/
+$svar=$_POST ["svar"];
+if ($svar == 9) /* avgitt svar er riktig */
 {
-    print("Riktig. 3 ganger 3 er 9");
+print("Riktig. 3 ganger 3 er 9 <br/>");
 }
-else
+else /* avgitt svar er feil */
 {
-    print("Feil. 3 ganger 3 er ikke $svar");
+print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
 }
 ?>
