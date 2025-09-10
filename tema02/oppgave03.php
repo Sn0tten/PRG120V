e466b970cb6ff7d106a405c3f0e814aaae9e2980
Sn0tten <king.ferdows4@gmail.com>
2025-09-10 13:36:36 +0200

    dwd

--- tema02/oppgave03.php
+++ tema02/oppgave03.php
@@ -1,23 +1,23 @@
 <?php
 $svar=$_POST ["svar"];
-$svar2=$_POST ["svar2"];
+$svar1=$_POST ["svar1"];
 if (!$svar || !$svar2) 
 {
 print("du har ikke svart på spørsmålene. <br/>");
 }
-else if ($svar == "j" && $svar2 == "j")
+else if ($svar == "j" && $svar1 == "j")
 {
     print ("du er gift og har barn");
 }
-else if ($svar == "n" && $svar2 == "n")
+else if ($svar == "n" && $svar1 == "n")
 {
     print ("du er ikke gift og har ikke barn");
 }
-else if ($svar == "j" && $svar2 == "n")
+else if ($svar == "j" && $svar1 == "n")
 {
     print ("du er gift og har ikke barn");
 }
-else if ($svar == "n" && $svar2 == "j")
+else if ($svar == "n" && $svar1 == "j")
 {
     print ("du er ikke gift og har barn");
 }
