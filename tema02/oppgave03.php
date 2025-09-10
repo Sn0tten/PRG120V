6b4eba329fcd356b4047426f41af06040898bde6
Sn0tten <king.ferdows4@gmail.com>
2025-09-10 13:33:37 +0200

    dv

--- tema02/oppgave03.php
+++ tema02/oppgave03.php
@@ -5,23 +5,23 @@ if (!$svar || !$svar2)
 {
 print("du har ikke svart på spørsmålene. <br/>");
 }
-else if ($svar == "j" && $svar2 == "j");
+else if ($svar == "j" && $svar2 == "j")
 {
     print ("du er gift og har barn");
 }
-else if ($svar == "n" && $svar2 == "n");
+else if ($svar == "n" && $svar2 == "n")
 {
     print ("du er ikke gift og har ikke barn");
 }
-else if ($svar == "j" && $svar2 == "n");
+else if ($svar == "j" && $svar2 == "n")
 {
     print ("du er gift og har ikke barn");
 }
-else if ($svar == "n" && $svar2 == "j");
+else if ($svar == "n" && $svar2 == "j")
 {
     print ("du er ikke gift og har barn");
 }
-else ($svar == "n" && $svar2 == "n");
+else ($svar == "n" && $svar2 == "n")
 {
     print ("du er ikke gift og har ikke barn");
 }
