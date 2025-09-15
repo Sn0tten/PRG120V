92f0e50a5254f864f8588186355fad49dd8690e0
Sn0tten <king.ferdows4@gmail.com>
2025-09-15 18:58:04 +0200

    66

--- tema03/oppgave04.php
+++ tema03/oppgave04.php
@@ -1,9 +1,10 @@
-<?php   
- $sum = 0;
-for ($tall =1;$tall<=10;$tall++)
-   
-    $sum += $tall;
-    {
-    print("summen av tallene 1 til 10 er $sum");
-    }
+<?php
+$sum = 0; // start på 0
+
+for ($tall = 1; $tall <= 10; $tall++) {
+    $sum += $tall;  // legg til tallet i summen
+}
+
+// utskrift SKAL være utenfor løkka
+print("Summen av tallene 1 til 10 er $sum");
 ?>
