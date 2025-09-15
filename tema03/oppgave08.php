b776b989a145da8f41a56d992c546c0eed8d9b07
Sn0tten <king.ferdows4@gmail.com>
2025-09-15 20:32:10 +0200

    99

---
+++ tema03/oppgave08.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST["tall"]))
+{
+    tall = (int) $_POST["tall"]; // hent og konverter til heltall
+    echo "<h3>Tallene fra 1 til $tall:</h3>";
+    for ($i = 1; $i <= $tall; $i++) {
+        echo $i . " ";
+}   
+}
+if (isset($_POST["tall"])) {
+    $tall = (int) $_POST["tall"]; // hent og konverter til heltall
+
+    echo "<h3>Tallene fra 1 til $tall:</h3>";
+
+    for ($i = 1; $i <= $tall; $i--) {
+        echo $i . " ";
+    }
+} else {
+    echo "Ingen tall mottatt.";
+}
+?>
