7d66a594e620d721067d852111a7fda0a6a874dc
Sn0tten <king.ferdows4@gmail.com>
2025-09-15 10:31:43 +0200

    w

--- tema03/oppgave01.php
+++ tema03/oppgave01.php
@@ -3,16 +3,13 @@ for ($i=1; $i <= 10; $i++)
 { 
     print ("$i <br/>");
 }
-for while ( $tall1= 1; $tall1 <= 10; $tall1++)
-{
-    print ("$tall1 <br/>");
-}
-
-
 $tall1=1;
 while ($tall1 <= 10)
 {
     print ("$tall1 <br/>");
     $tall1++;
 }
+startverdi: $tall1=1; 
+betingelse: $tall1 <= 10;
+øking: $tall1++;
 ?>
