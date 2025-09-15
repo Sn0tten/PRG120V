256eb1e8d0d820185ac505bfab85ddd8f9cc54e7
Sn0tten <king.ferdows4@gmail.com>
2025-09-15 20:16:03 +0200

    ee

--- tema03/oppgave07.php
+++ tema03/oppgave07.php
@@ -1,9 +1,13 @@
-<?php
- 
-$navn = array("Ferdows", "Rafeullah");
-$oppdeltnavn=explode(" ", $navn);
-$fornavn=$oppdeltnavn[0];
-$etternavn=$oppdeltnavn[1];
-print("Fornavn: $fornavn <br/>");
-print("Etternavn: $etternavn <br/>");
+
+<?php /* Oppgave 7 */
+/*
+/* Programmet mottar et navn fra et HTML-skjema
+/* Programmet skriver ut fornavn og etternavn hver for seg
+*/
+$navn=$_POST["navn"];
+$oppdeltNavn=explode(" ",$navn);
+$fornavn=$oppdeltNavn[0];
+$etternavn=$oppdeltNavn[1];
+print("Fornavnet er $fornavn <br/>");
+print("Etternavnet er $etternavn <br/>"); /* fornavn og etternavn skrevet ut hver for seg */
 ?>
