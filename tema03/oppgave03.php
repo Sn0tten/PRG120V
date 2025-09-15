595ec83c2820465ec69847f07aa004f3dc5122b0
Sn0tten <king.ferdows4@gmail.com>
2025-09-15 12:34:58 +0200

    wwe

---
+++ tema03/oppgave03.php
@@ -0,0 +1,13 @@
+<?php
+for ($i=1; $i <= 10; $i++) 
+{ 
+    print ("$i <br/>");
+}
+for ($i = 11; $i <= 20; $i++) {
+    echo $i . " har kvadratet " . ($i * $i) . "<br>";
+}
+for ($i = 21 ; $i <= 30; $i++) {
+    echo $i . " har kuben " . ($i * $i * $i) . "<br>";
+}
+?>
+-< --- IGNORE ---
