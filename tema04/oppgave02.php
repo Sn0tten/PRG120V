c933284e111b53b2e2d496f952621bfe2e8a0469
Sn0tten <king.ferdows4@gmail.com>
2025-09-19 16:19:13 +0200

    22

---
+++ tema04/oppgave02.php
@@ -0,0 +1,24 @@
+<?php
+// programmet mottar en klassekode som f.eks "PRG120V1" fra et html skjema//
+// programmet sjekker om klassekoden er korrekt fylt ut//
+$klassekode = $_POST["klassekode"];
+$lovligklassekode = true;
+if (!$klassekode) //klassekode er ikke fylt ut//
+{
+    $lovligklassekode = false;
+    echo "Klassekode er ikke fylt ut <br>";
+}
+else if (strlen($klassekode) != 8) //klassekode har ikke 8  tegn//
+{
+    $lovligklassekode = false;
+    echo "Klassekode må ha 8 tegn <br>";
+}
+else if (!ctype_alnum($klassekode)) //klassekode er ikke alfanumerisk//
+{
+    $lovligklassekode = false;
+    echo "Klassekode må være alfanumerisk <br>";
+}
+if ($lovligklassekode) {
+    echo "Klassekode er $klassekode <br>";
+}
+?>
