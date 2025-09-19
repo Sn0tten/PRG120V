a86f0e204450ebe3377d708cfdd6d52c02b2e49b
Sn0tten <king.ferdows4@gmail.com>
2025-09-19 16:12:36 +0200

    33

--- tema04/oppgave01.php
+++ tema04/oppgave01.php
@@ -1,13 +1,24 @@
 <?php
-$_POST = $postnummber = $poststed = "";
-$feil = false;
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-    
-    if (empty($_POST["postnummber"])) {
-        $feil = true;
-    } else {
-        $postnummber = test_input($_POST["postnummber"]);
-        if (!preg_match("/^[0-9]{4}$/", $postnummber)) {
-            $feil = true;
-        }
-    }  
+// programmet mottar postnummer fra et html skjema//
+// programmet sjekker om postnummeret er korrekt fylt ut//
+$postnummer = $_POST["postnummer"];
+$lovligpostnummer = true;
+if (!$postnummer) //postnummer er ikke fylt ut//
+{
+    $lovligpostnummer = false;
+    echo "Postnummer er ikke fylt ut <br>";
+}
+else if (strlen($postnummer) != 4) //postnummer har ikke 4 siffer//
+{
+    $lovligpostnummer = false;
+    echo "Postnummer må ha 4 siffer <br>";
+}
+else if (!is_numeric($postnummer)) //postnummer er ikke et tall//
+{
+    $lovligpostnummer = false;
+    echo "Postnummer må være et tall <br>";
+}
+if ($lovligpostnummer) {
+    echo "Postnummer er $postnummer <br>";
+}
+?>
