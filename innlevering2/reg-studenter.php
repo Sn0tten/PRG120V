917a5fb99f80adaa768d3f57719184faa7d3ff3d
Sn0tten <king.ferdows4@gmail.com>
2025-09-29 12:28:45 +0200

    3

--- innlevering2/reg-studenter.php
+++ innlevering2/reg-studenter.php
@@ -1,40 +1,51 @@
-<?php /* registrer student med dynamiske funksjoner */
-/*  Programmet registrerer en ny student i databasen
+<?php
+/* registrer-student-dynamisk
+   HTML-skjema for å registrere en student med dynamisk klassekode-liste
 */
-?>
-<?php include "db-tilkobling.php"; ?>
-<?php include "dynamiske-funksjoner.php"; ?>
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <title>Registrer student</title>
-</head>
-<body>
-    <h1>Registrer ny student</h1>
-    <form method="post">
-        Brukernavn: <input type="text" name="brukernavn" required><br>
-        Fornavn: <input type="text" name="fornavn" required><br>
-        Etternavn: <input type="text" name="etternavn" required><br>
-        Klassekode: 
-        <?php lagDropdown("klasse", "klassekode", "klassekode"); ?>
-        <input type="submit" value="Registrer">
-    </form>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $brukernavn = $_POST["brukernavn"];
-        $fornavn = $_POST["fornavn"];
-        $etternavn = $_POST["etternavn"];
-        $klassekode = $_POST["klassekode"];
-        $sql = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode) 
-                VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode')";
-        if ($conn->query($sql)) {
-            echo "<p>Student registrert!</p>";
+
+include("db-tilkobling.php"); 
+include("dynamiske-funksjoner.php"); 
+?> 
+
+<h3>Registrer student</h3>
+
+<form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
+  Brukernavn: <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+  Fornavn: <input type="text" id="fornavn" name="fornavn" required /> <br/>
+  Etternavn: <input type="text" id="etternavn" name="etternavn" required /> <br/>
+  Klassekode: 
+  <select name="klassekode" required>
+      <option value="">-- Velg klasse --</option>
+      <?php listeboksklassekode(); ?>
+  </select> <br/>
+  
+  <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
+<?php 
+if (isset($_POST["registrerStudentKnapp"])) {
+    $brukernavn = $_POST["brukernavn"];
+    $fornavn    = $_POST["fornavn"];
+    $etternavn  = $_POST["etternavn"];
+    $klassekode = $_POST["klassekode"];
+
+    if (!$brukernavn || !$fornavn || !$etternavn || !$klassekode) {
+        print ("Alle felt må fylles ut");
+    } else {
+        // Sjekk om studenten allerede finnes
+        $sqlSetning = "SELECT * FROM student WHERE brukernavn='$brukernavn';";
+        $sqlResultat = mysqli_query($db, $sqlSetning) or die ("ikke mulig å hente data fra databasen");
+        $antallRader = mysqli_num_rows($sqlResultat);
+
+        if ($antallRader != 0) {  
+            print ("Studenten er registrert fra før");
         } else {
-            echo "<p>Feil: " . $conn->error . "</p>";
+            $sqlSetning = "INSERT INTO student VALUES('$brukernavn','$fornavn','$etternavn','$klassekode');";
+            mysqli_query($db, $sqlSetning) or die ("ikke mulig å registrere data i databasen");
+
+            print ("Følgende student er nå registrert: $brukernavn $fornavn $etternavn ($klassekode)"); 
         }
     }
-    ?>
-    <p><a href="index.html">Tilbake til meny</a></p>
-</body>
-</html>
+}
+?> 
