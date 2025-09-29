834f32cb731d6478f704446c62ea017b91d26f80
Sn0tten <king.ferdows4@gmail.com>
2025-09-29 12:19:47 +0200

    76371

--- innlevering2/dynamiske-funksjoner.php
+++ innlevering2/dynamiske-funksjoner.php
@@ -1,49 +1,14 @@
 <?php
-/* dynamiske-funksjoner.php
-   Denne filen inneholder dynamiske funksjoner:
-   - listeboksklassekode()
-   - listeboksbrukernavn()
-   - sjekkbokserklassekode()
-*/
-
-function listeboksklassekode()
-{
-    include("db.php"); // tilkobling til database
-    $sql = "SELECT * FROM klasse ORDER BY klassekode;";
-    $resultat = $conn->query($sql) or die("Ikke mulig å hente data fra databasen");
-
-    while ($rad = $resultat->fetch_assoc()) {
-        $klassekode = $rad["klassekode"];
-        $klassenavn = $rad["klassenavn"];
-        $studiumkode = $rad["studiumkode"];
-        print("<option value='$klassekode'>$klassekode - $klassenavn ($studiumkode)</option>");
-    }
-}
-
-function listeboksbrukernavn()
-{
-    include("db.php"); // tilkobling til database
-    $sql = "SELECT * FROM student ORDER BY brukernavn;";
-    $resultat = $conn->query($sql) or die("Ikke mulig å hente data fra databasen");
-
-    while ($rad = $resultat->fetch_assoc()) {
-        $brukernavn = $rad["brukernavn"];
-        $fornavn = $rad["fornavn"];
-        $etternavn = $rad["etternavn"];
-        print("<option value='$brukernavn'>$brukernavn - $fornavn $etternavn</option>");
-    }
-}
-
-function sjekkbokserklassekode()
-{
-    include("db.php"); // tilkobling til database
-    $sql = "SELECT * FROM klasse ORDER BY klassekode;";
-    $resultat = $conn->query($sql) or die("Ikke mulig å hente data fra databasen");
-
-    while ($rad = $resultat->fetch_assoc()) {
-        $klassekode = $rad["klassekode"];
-        $klassenavn = $rad["klassenavn"];
-        print("<input type='checkbox' name='klassekode[]' value='$klassekode'> $klassekode - $klassenavn<br>");
+function ListeboksKlassekode() {
+    include "db-tilkobling.php";
+    $sql = "SELECT klassekode FROM klasse ORDER BY klassekode;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_array($resultat)) {
+            $kode = $rad["klassekode"];
+            $navn = $rad["klassenavn"];
+            echo "<option value='$klassekode'>$klassekode</option>";
+        }
     }
 }
 ?>
