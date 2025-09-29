35f6aa780b53f0b2483045e304a370ca5048425c
Sn0tten <king.ferdows4@gmail.com>
2025-09-29 12:14:46 +0200

    66

--- innlevering2/vis-klasse.php
+++ innlevering2/vis-klasse.php
@@ -1,26 +1,25 @@
-<?php include "db.php"; ?>
+<?php /* vis alle klasser */
+/*  Programmet henter alle klasser fra databasen og viser dem i en tabell
+*/
 
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <title>Vis klasser</title>
-</head>
-<body>
-    <h1>Alle klasser</h1>
-    <table border="1">
-        <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
-        <?php
-        $res = $conn->query("SELECT * FROM klasse");
-        while ($row = $res->fetch_assoc()) {
-            echo "<tr>
-                    <td>".$row["klassekode"]."</td>
-                    <td>".$row["klassenavn"]."</td>
-                    <td>".$row["studiumkode"]."</td>
-                  </tr>";
+    include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+    
+    $sqlSetning="SELECT * FROM klasse ORDER BY klassekode;";
+    $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    $antallRader=mysqli_num_rows($sqlResultat); 
+    
+    print ("<h3>Alle klasser</h3>");
+    print ("<table border='1'>");
+    print ("<tr><th>klassekode</th><th>klassenavn</th><th>studiumkode</th></tr>");
+    
+    for ($r=1;$r<=$antallRader;$r++)
+        {
+        $rad=mysqli_fetch_array($sqlResultat); 
+        $klassekode=$rad["klassekode"];
+        $klasssenavn=$rad["klasssenavn"];
+        $studiumkode=$rad["studiumkode"];
+        
+        print ("<tr><td>$klassekode</td><td>$klasssenavn</td><td>$studiumkode</td></tr>");
         }
-        ?>
-    </table>
-    <p><a href="index.html">Tilbake til meny</a></p>
-</body>
-</html>
+    print ("</table>");
+?>
--- innlevering2/vis-student.php
+++ innlevering2/vis-student.php
@@ -1,29 +1,25 @@
-<?php include "db-tilkobling.php"; ?>
-
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <title>Vis studenter</title>
-</head>
-<body>
-    <h1>Alle studenter</h1>
-    <table="1">
-        <tr><th>Brukernavn</th><th>Fornavn</th><th>Etternavn</th><th>Klasse</th></tr>
-        <?php
-        $res = $conn->query("SELECT s.brukernavn, s.fornavn, s.etternavn, k.klassenavn 
-                             FROM student s 
-                             JOIN klasse k ON s.klassekode = k.klassekode");
-        while ($row = $res->fetch_assoc()) {
-            echo "<tr>
-                    <td>".$row["brukernavn"]."</td>
-                    <td>".$row["fornavn"]."</td>
-                    <td>".$row["etternavn"]."</td>
-                    <td>".$row["klassenavn"]."</td>
-                  </tr>";
+<?php /* vis student */
+/*  Programmet henter alle studenter fra databasen og viser dem i en tabell
+*/
+    include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+    
+    $sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
+    $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    $antallRader=mysqli_num_rows($sqlResultat); 
+    
+    print ("<h3>Alle studenter</h3>");
+    print ("<table border='1'>");
+    print ("<tr><th>brukernavn</th><th>fornavn</th><th>etternavn</th><th>klassekode</th></tr>");
+    
+    for ($r=1;$r<=$antallRader;$r++)
+        {
+        $rad=mysqli_fetch_array($sqlResultat); 
+        $brukernavn=$rad["brukernavn"];
+        $fornavn=$rad["fornavn"];
+        $etternavn=$rad["etternavn"];
+        $klassekode=$rad["klassekode"];
+        
+        print ("<tr><td>$brukernavn</td><td>$fornavn</td><td>$etternavn</td><td>$klassekode</td></tr>");
         }
-        ?>
-    </table>
-    <p><a href="index.html">Tilbake til meny</a></p>
-</body>
-</html>
+    print ("</table>");
+?>
