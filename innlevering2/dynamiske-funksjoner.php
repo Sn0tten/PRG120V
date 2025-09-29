f5011e8c0695c2c0678943c676bd140f156b847a
Sn0tten <king.ferdows4@gmail.com>
2025-09-29 11:57:43 +0200

    3

--- innlevering2/dynamiske-funksjoner.php
+++ innlevering2/dynamiske-funksjoner.php
@@ -6,22 +6,35 @@
 */
 ?>
 <?php
-function listeboksklassekode()
+function listeboksbrukernavn ()
 {
-  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
-      
-  $sqlSetning="SELECT * FROM klassenavn ORDER BY klassekode;";
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
-    /* SQL-setning sendt til database-serveren */
-    
-  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
-
-  for ($r=1;$r<=$antallRader;$r++)
-    {
-      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
-      $klassekode=$rad["klassekode"]; 
-      $klassenavn=$rad["klassenavn"];
-
-      print("<option value='$klassekode'>$klassekode $klassenavn</option>");  /* ny verdi i listeboksen laget */
-    }
+include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
+$sqlSetning="SELECT * FROM studium ORDER BY studiumkode;";
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
+for ($r=1;$r<=$antallRader;$r++)
+{
+$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
+$studiumkode=$rad["studiumkode"];
+$studiumnavn=$rad["studiumnavn"];
+print("<option value='$studiumkode'>$studiumkode $studiumnavn </option>"); /* ny verdi i listeboksen
+laget */
 }
+}
+function listeboksklassekode ()
+{
+include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
+$sqlSetning="SELECT * FROM emne ORDER BY emnekode;";
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
+for ($r=1;$r<=$antallRader;$r++)
+{
+$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
+$emnekode=$rad["emnekode"];
+$emnenavn=$rad["emnenavn"];
+$studiumkode=$rad["studiumkode"];
+print("<option value='$emnekode'>$emnekode $emnenavn </option>"); /* ny verdi i listeboksen laget
+*/
+}
+}
+?>
